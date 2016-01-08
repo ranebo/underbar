8c72676fc4f3bd188dcc43f662925061753f0d81
Rane Gridley <ranegri3@gmail.com>
2016-01-08 00:34:31 -0800

    _.every COMPLETED
    All tests passed

--- src/underbar.js
+++ src/underbar.js
@@ -201,6 +201,17 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    if (iterator === undefined){
+      iterator = _.identity;
+    }
+    return _.reduce(collection, function(accumulator, item){
+        if (!iterator(item)){
+          return false;
+        }
+        else {
+          return accumulator;
+        }
+    },true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
