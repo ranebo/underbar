89970d651a2d1d6f9b478b6f4df22284c2be0363
Rane Gridley <ranegri3@gmail.com>
2016-01-08 22:19:48 -0800

    _.extend COMPLETED
    All tests passed
    Commented out my first version using logic, then re-made to use _.each

--- src/underbar.js
+++ src/underbar.js
@@ -246,7 +246,26 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(argument){
+      _.each(argument, function (value, key){
+        obj[key] = value;
+      });
+    });
+    return obj;
   };
+
+  /*
+  THIS WAS MY FIRST SOLVE: Then noticed I could use _.each.
+
+  _.extend = function(obj) {
+    for (var i = 0; i < arguments.length; i++){
+        for (var prop in arguments[i]){
+          obj[prop] = arguments[i][prop];
+        }
+      }
+      return obj;
+  };
+  */
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
