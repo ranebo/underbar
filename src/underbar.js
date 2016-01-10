fbab25188492ca9da67c4b3405ab9cd03a4fcb64
Rane Gridley <ranegri3@gmail.com>
2016-01-10 14:36:30 -0800

    _.delay COMPLETED
    All tests passed
    -Look into better way to handle arguments that must be passed to the function being timed

--- src/underbar.js
+++ src/underbar.js
@@ -357,6 +357,14 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = [];
+
+    for (var i = 2; i < arguments.length; i++){
+      args[i-2] = arguments[i];
+    }
+
+    setTimeout(function(){
+      func.apply(this, args)} , wait);
   };
 
 
